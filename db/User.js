0b3255ce318c9831a95d3aa9149fad447a74768e
unknown <playbox8g@gmail.com>
2018-12-30 12:45:21 +0800

    auth push

--- db/User.js
+++ db/User.js
@@ -1,6 +1,7 @@
 const {mongoose} = require('./mongoose.js');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -30,6 +31,11 @@ var UserSchema = new mongoose.Schema({
 		}
 	}]
 });
+
+UserSchema.methods.toJSON = function () {
+	var userObject = this.toObject();
+	return _.pick(userObject, ['_id', 'email']);
+}
 
 UserSchema.methods.generateAuthToken = function () {
 	var access = 'auth';
