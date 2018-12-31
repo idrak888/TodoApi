d66f60fb24cc3e87f696075e7029b090dd9f8b1f
unknown <playbox8g@gmail.com>
2018-12-31 17:08:01 +0800

    Commit 31

--- db/User.js
+++ db/User.js
@@ -57,23 +57,27 @@ UserSchema.statics.findByToken = function (token) {
 		return Promise.reject();
 	}
 
-	return User.findOne({_id:decoded._id, 'tokens.token':token, 'tokens.access':'auth'})
+	return User.findOne({
+		'_id':decoded._id, 
+		'tokens.token':token, 
+		'tokens.access':'auth'
+	});
 };
 
-UserSchema.pre('save', function (next) {
-	var user = this;
+// UserSchema.pre('save', function (next) {
+// 	var user = this;
 
-	if(user.isModified('password')) {
-		bcrypt.genSalt(10, (err, salt) => {
-			bcrypt.hash(user.password, salt, (err, hash) => {
-				user.password = hash;
-				next();
-			});
-		});
-	}else {
-		next();
-	}
-});
+// 	if(user.isModified('password')) {
+// 		bcrypt.genSalt(10, (err, salt) => {
+// 			bcrypt.hash(user.password, salt, (err, hash) => {
+// 				user.password = hash;
+// 				next();
+// 			});
+// 		});
+// 	}else {
+// 		next();
+// 	}
+// });
 
 var User = mongoose.model('User', UserSchema);
 
