1e656a61138565372853b3c958b56e3e2912b981
unknown <playbox8g@gmail.com>
2018-12-31 18:07:42 +0800

    Commit 31

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/users/login', (req, res) => {
 	User.findOne({email:body.email}).then(user => {
 		if (user.password === body.password) {
 			var token = user.generateAuthToken();
-			res.send(token);
+			res.header('x-auth', token).send(user);
 		}else {
 			res.status(401).send('Wrong password or email.');
 		}
