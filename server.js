09188d7bf8241fc0313ff0d17f0ce18e208b9efa
unknown <playbox8g@gmail.com>
2018-12-31 12:05:09 +0800

    hash

--- server.js
+++ server.js
@@ -65,16 +65,6 @@ app.post('/users', (req, res) => {
 	});
 });
 
-app.post('/users/login', (req, res) => {
-	var body = _.pick(req.body, ['email', 'password']);
-	
-	User.findByCredentials(body.email, body.password).then((user) => {
-		res.send(user);
-	}).catch((e) => {
-		res.status(400).send();
-	});
-});
-
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
