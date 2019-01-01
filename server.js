1d2133c7e8f6400ffb064b7c099f92e8f9c2e465
unknown <playbox8g@gmail.com>
2019-01-01 20:45:23 +0800

    1-1-2019

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ var app = express();
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Expose-Headers", "X-Auth");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-	);
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth");
 	if (req.method === 'OPTIONS') {
 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
 	}
